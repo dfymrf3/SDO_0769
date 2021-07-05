46ce8794aea0fdd0afcbb328b677abb6cf6c1a9b
dfymrf3 <86723690+dfymrf3@users.noreply.github.com>
2021-07-05 15:37:18 +0300

    Update Bot_for-Yandex.js
    Переделал на другой сайт,и немного изменил массив.Так всё работает на яндексе. Спасибо за головоломку)На самом деле это очень интересно

--- Bot_for-Yandex.js
+++ Bot_for-Yandex.js
@@ -4,23 +4,23 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Kormilitsyn Ivan
-// @match        https://www.google.com/*
+// @match        https://yandex.ru/*
 // @grant        none
 // ==/UserScript==
-let keywords = ["валторна","гобой","Аэрофоны","Граммофонная пластинка"];
+let keywords = ["валторна","Аэрофоны","Граммофонная пластинка"];
 let keyword = keywords[getRandom(0,keywords.length)];
 
-document.getElementsByName('q')[0].value = keyword;
+document.getElementsByName('text')[0].value = keyword;
 
-let btnK = document.getElementsByName('btnK')[0];
+let btnK = document.getElementsByClassName('button mini-suggest__button')[0];
 let links = document.links;
 
 if(btnK !== undefined) {
-document.getElementsByName('q')[0].value = keyword;
-document.getElementsByName('btnK')[0].click();
+document.getElementsByName('text')[0].value = keyword;
+document.getElementsByClassName('button mini-suggest__button')[0].click();
 } else {
     for(let i = 0; i < links.length; i++) {
-        if (links[i].href.includes('belcanto.ru')) {
+        if (links[i].href.includes('dic.academic.ru')) {
             let link = links[i];
             link.click();
             break;
@@ -31,6 +31,7 @@ document.getElementsByName('btnK')[0].click();
 function getRandom(min,max) {
 return Math.floor(Math.random()* (max-min)+min)
 }
+
 
 
 
